d862f0695bf08307e5f527688b40d13de4d4a061
alexi2020 <alexidiaz123alexi@gmail.com>
2020-07-09 20:26:04 -0400

    initial proyect commit
    Proyecto dos escenas bosque

---
+++ Assets/scripts/move.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 4f;
+    Vector2 mov;
+    Rigidbody2D rb;
+    Animator anim;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mov = new Vector2(
+            Input.GetAxisRaw("Horizontal"),
+            Input.GetAxisRaw("Vertical"));
+        if(mov!= Vector2.zero)
+        {
+            anim.SetFloat("moveX",mov.x);
+            anim.SetFloat("moveY", mov.y);
+        }
+        
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + mov * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/scripts/moveRight.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveRight : MonoBehaviour { 
+    public float maxspeed;
+    Rigidbody2D caballero;
+    Animator anim;
+  
+   
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        caballero=GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+     
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move = Input.GetAxis("Horizontal");
+        caballero.velocity = new Vector2(move * maxspeed, caballero.velocity.y);
+
+        anim.SetFloat("velMoviment", move);
+
+
+
+    }
+    
+ 
+        
+    
+}
